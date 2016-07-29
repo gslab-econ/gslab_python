2c07ced0337781f7d6146d0180ad4d1fe83d1600
Shun-Yang <yangf2@carleton.edu>
2016-07-29 13:24:16 -0700

    #16 first try

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='gslab',
+      version='0.7',
+      description='python package for GSLab',
+      url='git@github.com:gslab-econ/GSLab.git',
+      author='Frank',
+      author_email='yangf2@carleton.edu',
+      license='MIT',
+      packages=['gslab'],
+      zip_safe=False)
