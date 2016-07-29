40b5e04e67e8a4d577686d2f0bb92f0a492e9f8f
Adam Rosenberg <arosenbe@stanford.edu>
2016-07-29 10:18:50 -0700

    #14 remove anonymous retaler directory

--- anonymous_retailer/py/input_params.py
+++
@@ -1,9 +0,0 @@
-# We consider scans with spending or quantity outside these values to have been
-# errors or not made by individual customers
-
-MIN_QUANTITY_PER_SCAN = -500
-MAX_QUANTITY_PER_SCAN = 5000
-MIN_SPEND_PER_SCAN = -3000
-MAX_SPEND_PER_SCAN = 10000
-TRANSACTIONS_START_MONTH = '200601' 
-TRANSACTIONS_END_MONTH = '201301'
