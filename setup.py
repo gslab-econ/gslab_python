63addf111b81df9596c6ef3413b28cff114b0d0c
Shun-Yang <yangf2@carleton.edu>
2016-08-03 11:54:21 -0700

    update readme

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
-setup(name='gslab',
-      version='1.0',
-      description='python package for GSLab',
-      url='git@github.com:gslab-econ/GSLab.git',
-      author='GSLab',
-      author_email='GSLab@stanford.edu',
+setup(name='GSLab_Tools',
+      version='1.0.1',
+      description='python tools for GSLab',
+      url='https://github.com/gslab-econ/gslab_python',
+      author='Matthew Gentzkow, Jesse Shapiro',
+      author_email='gentzkow@stanford.edu, jesse_shapiro_1@brown.edu',
       license='MIT',
       packages = find_packages(),
       zip_safe=False)
