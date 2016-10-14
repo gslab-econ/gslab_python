0baa67b4b0038c796f28aff93c30150f6703279d
Levi Boxell <leviboxell@gmail.com>
2016-10-14 10:14:24 -0700

    Update setup.py

--- gencat/setup.py
+++ gencat/setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(name             = 'GSLab_Gencat',
-      version          = '0.0.1',
+      version          = '0.0.2',
       description      = 'A class to unzip and concatenate, and rezip files.',
-      url              = 'https://github.com/gslab-econ/gslab_python/tree/issue11-gencat/gencat',
+      url              = 'https://github.com/gslab-econ/gslab_python/tree/issue13-subgroupzip/gencat',
       author           = 'Matthew Gentzkow, Jesse Shapiro',
       author_email     = 'gentzkow@stanford.edu, jesse_shapiro_1@brown.edu',
       license          = 'MIT',
