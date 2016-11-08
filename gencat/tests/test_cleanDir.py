b9466be7c050f4ba4804d0106a19ab15b80a003a
Adam Rosenberg <arosenbe@stanford.edu>
2016-11-08 13:26:04 -0800

    #17 add comments to test_cleanDir.py

--- gencat/tests/test_cleanDir.py
+++ gencat/tests/test_cleanDir.py
@@ -21,19 +21,44 @@ class test_cleanDir(unittest.TestCase):
                 shutil.rmtree(path, ignore_errors = True)
                 os.makedirs(path)
 
-    def test_base(self):
+    def test_cleandir(self):
+        '''
+        Test that files in directory are cleared
+        '''
+        with open('test_data/test_file', 'wb') as f:
+            f.write('test')
+        testcat.cleanDir('./test_data')
+        self.assertFalse(os.listdir('./test_data'))
+    
+    def test_makedir(self):
+        '''
+        Test that method creates a new directory.
+        '''
+        try:
+            shutil.rmtree('./test')
+        except:
+            pass
         testcat.cleanDir('./test')
         self.assertTrue(os.path.isdir('./test'))
     
     def test_strange(self):
-        testcat.cleanDir('./test', new_dir = 'gibberish')
+        '''
+        Test that new_dir flag does not affect output unless False is entered
+        '''
+        testcat.cleanDir('./test', new_dir = 'gibberish1!')
         self.assertTrue(os.path.isdir('./test'))
     
     def test_nonew(self):
+        '''
+        Test that a new directory is not created if the new_dir flag is False
+        '''
         testcat.cleanDir('./test', new_dir = False)
         self.assertFalse(os.path.isdir('./test'))
     
     def test_deeppath(self):
+        '''
+        Test that the cleanDir method functions on subdirectories
+        '''
         testcat.cleanDir('./a/deep/test/dir')
         self.assertTrue(os.path.isdir('./a/deep/test/dir'))
         shutil.rmtree('./a')
