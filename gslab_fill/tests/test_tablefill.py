beaa68cdbca62fec8f37bd7fdb13a72d383f276f
M-R-Sullivan <m.r.sullivan@stanford.edu>
2016-11-16 09:31:59 -0800

    #25: Modified structure for  gslab_fill unit tests

--- gslab_fill/tests/test_tablefill.py
+++ gslab_fill/tests/test_tablefill.py
@@ -1,8 +1,13 @@
 #! /usr/bin/env python
 
-import unittest, sys, os, re, decimal
+import unittest
+import sys
+import os
+import re
+import decimal
 from subprocess import check_call, CalledProcessError
-sys.path.append('../..') 
+
+sys.path.append('..') 
 from tablefill import tablefill
 from nostderrout import nostderrout
 
@@ -10,9 +15,9 @@ class testTablefill(unittest.TestCase):
 
     def testInput(self):
         with nostderrout():
-            message = tablefill(input = './input/tables_appendix.txt ./input/tables_appendix_two.txt', \
+            message = tablefill(input    = './input/tables_appendix.txt ./input/tables_appendix_two.txt', \
                                 template = './input/tablefill_template.lyx', \
-                                output = './input/tablefill_template_filled.lyx')
+                                output   = './input/tablefill_template_filled.lyx')
         self.assertIn('filled successfully', message)                        
         tag_data = open('./input/tablefill_template.lyx', 'rU').readlines()
         filled_data = open('./input/tablefill_template_filled.lyx', 'rU').readlines()
@@ -39,48 +44,49 @@ class testTablefill(unittest.TestCase):
    
     def testBreaksRoundingString(self):
         with nostderrout():
-            error = tablefill(input =  './input/tables_appendix.txt ./input/tables_appendix_two.txt', \
-                              template =  './input/tablefill_template_breaks.lyx', \
-                              output =  './input/tablefill_template_filled.lyx')
+            error = tablefill(input    = './input/tables_appendix.txt ./input/tables_appendix_two.txt', \
+                              template = './input/tablefill_template_breaks.lyx', \
+                              output   = './input/tablefill_template_filled.lyx')
         self.assertIn('InvalidOperation', error)
     
     def testIllegalSyntax(self):
         # missing arguments
         with nostderrout():
-            error = tablefill(input = './input/tables_appendix.txt ./input/tables_appendix_two.txt', \
-                              template = '../test/input/textfill_template.lyx')
+            error = tablefill(input    = './input/tables_appendix.txt ./input/tables_appendix_two.txt', \
+                              template = './input/textfill_template.lyx')
         self.assertIn('KeyError', error)
                 
         # non-existent input 1
         with nostderrout():
-            error = tablefill(input = './input/fake_file.txt ./input/tables_appendix_two.txt', \
-                              template =  './input/tablefill_template_breaks.lyx', \
-                              output =  './input/tablefill_template_filled.lyx')
+            error = tablefill(input    = './input/fake_file.txt ./input/tables_appendix_two.txt', \
+                              template = './input/tablefill_template_breaks.lyx', \
+                              output   = './input/tablefill_template_filled.lyx')
         self.assertIn('IOError', error)
         
         # non-existent input 2
         with nostderrout():
-            error = tablefill(input = './input/tables_appendix.txt ./input/fake_file.txt', \
-                              template =  './input/tablefill_template_breaks.lyx', \
-                              output =  './input/tablefill_template_filled.lyx')
+            error = tablefill(input    = './input/tables_appendix.txt ./input/fake_file.txt', \
+                              template = './input/tablefill_template_breaks.lyx', \
+                              output   = './input/tablefill_template_filled.lyx')
         self.assertIn('IOError', error)
         
     def testArgumentOrder(self):
         with nostderrout():
-            message = tablefill(input = './input/tables_appendix.txt ./input/tables_appendix_two.txt', \
-                                output = './input/tablefill_template_filled.lyx', \
+            message = tablefill(input    = './input/tables_appendix.txt ./input/tables_appendix_two.txt', \
+                                output   = './input/tablefill_template_filled.lyx', \
                                 template = './input/tablefill_template.lyx')
         self.assertIn('filled successfully', message)
         filled_data_args1 = open('./input/tablefill_template_filled.lyx', 'rU').readlines()
         
         with nostderrout():
-            message = tablefill(output = './input/tablefill_template_filled.lyx', \
+            message = tablefill(output   = './input/tablefill_template_filled.lyx', \
                                 template = './input/tablefill_template.lyx', \
-                                input = './input/tables_appendix.txt ./input/tables_appendix_two.txt')
+                                input    = './input/tables_appendix.txt ./input/tables_appendix_two.txt')
         self.assertIn('filled successfully', message)
         filled_data_args2 = open('./input/tablefill_template_filled.lyx', 'rU').readlines()
         
         self.assertEqual(filled_data_args1, filled_data_args2)
+        
         
 if __name__ == '__main__':
     os.getcwd()
