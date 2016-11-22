8b729fba82eb58d122c231aa58ca1db8162002c0
Michael Sullivan <m.r.sullivan@stanford.edu>
2016-11-22 09:14:50 -0800

    Update __init__.py

--- gslab_fill/tests/__init__.py
+++ gslab_fill/tests/__init__.py
@@ -1,8 +1,12 @@
 '''
-This directory contains unit tests for gslab_fill.
-It formerly executed logs_for_textfill.do, which is 
-now stored in the subdirectory /input/, to build 
-the following inputs for the unit tests:
+This directory contains unit tests for `gslab_fill`. These tests can be run using
+`python -m unittest discover`
+from `gslab_fill/` or from `gslab_fill/tests/`. To run the tests with logging, use:
+`python run_all_tests.py`
+from `gslab_scons/tests/`.
+
+The following inputs of these tests were created by running logs_for_textfill.do, 
+a Stata do file that is now stored in the subdirectory.
 - legal.log
 - alternative_prefix.log
 - tags_not_closed.log
