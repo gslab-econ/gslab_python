631f4dd66920fb14293f12051f1d03b75dcfda1e
chuanyu <yuchuan2016@gmail.com>
2017-01-24 14:54:03 -0800

    change version number

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ class CleanRepo(build_py):
 requirements = ['requests']
 
 setup(name         = 'GSLab_Tools',
-      version      = '3.0.1',
+      version      = '3.0.2',
       description  = 'Python tools for GSLab',
       url          = 'https://github.com/gslab-econ/gslab_python',
       author       = 'Matthew Gentzkow, Jesse Shapiro',
