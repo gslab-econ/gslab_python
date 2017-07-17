522f4ef5a79a423546ed27f1a8d115b811e68d6d
Quan Le <lequan@stanford.edu>
2017-07-16 20:30:21 -0700

    #68 remove prompt for github token

--- gslab_scons/misc.py
+++ gslab_scons/misc.py
@@ -245,9 +245,6 @@ def load_yaml_value(path, key):
 
     if key == "stata_executable":
         prompt = "Enter %s value or None to search for defaults: "
-    elif key == "github_token":
-        prompt = "(Optional) Enter %s to be stored in user_config.yaml.\n" + \
-                 "Github token can also be entered without echoing later:" 
     else:
         prompt = "Enter %s value: "
 
@@ -276,11 +273,14 @@ def load_yaml_value(path, key):
         else:
             return yaml_contents[key]
     except:
-        with open(path, 'ab') as f:        
-            val = str(raw_input(prompt % key))
-            if re.sub('"', '', re.sub('\'', '', val.lower())) == "none":
-                val = None
-            f.write('%s: %s\n' % (key, val))
+        if key == "github_token_optional":
+            val = None
+        else:
+            with open(path, 'ab') as f:        
+                val = str(raw_input(prompt % key))
+                if re.sub('"', '', re.sub('\'', '', val.lower())) == "none":
+                    val = None
+                f.write('%s: %s\n' % (key, val))
         return val
 
 
