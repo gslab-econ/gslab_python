51b3d204d58e4b04cb919fd89841645a6ac102e6
Shun-Yang <yangf2@carleton.edu>
2017-08-28 10:41:29 -0700

    edit readme

--- gslab_misc/__init__.py
+++ gslab_misc/__init__.py
@@ -3,9 +3,6 @@ gslab_misc: A library for miscellaneous python tools
 ============================================================================================
 Current tools: 
 		gencat: a Python library for concatenating text files. 
-				The gencat (General Concatenator) library defines an abstract class for 
-				concatenating text files. Please see this class's docstring for additional
-				information on its structure and functionalities. 
 '''
 
 from .gencat.gencat import gencat
