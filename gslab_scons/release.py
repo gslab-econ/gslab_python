53d4026992d231d9371874338f410d6cc5c38373
Adam Rosenberg <arosenbe@stanford.edu>
2017-09-06 16:44:45 -0700

    Pull request for #126: Fix lingering issues with release.py (#127)
    * #126 hit all checks, no documentation
    
    * #126 fix overwrite of scons_local_path
    
    * #126 docs
    
    * #127 #126 small readme changes for PR
    
    * #126 small fixes
    
    * #127 #126 abstract text search, better config checks
    
    * #127 #126 search dir not output for SConstruct

--- gslab_scons/release.py
+++ gslab_scons/release.py
@@ -12,8 +12,20 @@ def main(version = None,
          prov_excluded_dirs = [],
          external_provenance = [],
          dont_zip = False,
-         readme = None):
-    inspect_repo()
+         readme = None, 
+         scons_local_path = 'run.py'):
+
+    # Check if user specified a scons_local_path
+    if scons_local_path == 'run.py':
+        check_none = lambda scons_local_path, regex: bool(re.match(regex, scons_local_path, re.IGNORECASE))
+        try:
+            scons_local_path = next(arg for arg in sys.argv if re.search('^scons_local_path=', arg))
+            scons_local_path = re.sub('^scons_local_path=', '', scons_local_path)
+            if check_none(scons_local_path, 'None') or check_none(scons_local_path, 'False'):
+                scons_local_path = None
+        except:
+            pass
+    inspect_repo(scons_local_path = scons_local_path)
 
     # Extract information about the clone from its .git directory
     try: 
@@ -100,14 +112,16 @@ def main(version = None,
                            provenance_path   = provenance_path)
 
 
-def inspect_repo():
+def inspect_repo(scons_local_path = None):
     '''Ensure the repo is ready for release.'''
-    if not _release_tools.up_to_date(mode = 'scons'):
+    if not _release_tools.up_to_date(mode = 'scons', scons_local_path = scons_local_path):
         raise ReleaseError('SCons targets not up to date.')  
     elif not _release_tools.up_to_date(mode = 'git'):
-        print "Warning: `scons` has run since your latest git commit.\n"
+        warn = "Warning: your git working tree is not clean.\n" \
+               + "End this process and run `git status` for more information."
+        print warn
         response = raw_input("Would you like to continue anyway? (y|n)\n")
-        if response in ['N', 'n']: 
+        if response in ['N', 'n']:
             sys.exit()
 
 
