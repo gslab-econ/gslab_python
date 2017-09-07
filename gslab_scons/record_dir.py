ad9a389f7020c413cf3d5ea6840d60b897850ccb
Adam Rosenberg <arosenbe@stanford.edu>
2017-09-07 11:21:31 -0700

    #132 #131 better path checks, fix walk-break rule, new path names

--- gslab_scons/record_dir.py
+++ gslab_scons/record_dir.py
@@ -4,55 +4,81 @@ import mmh3
 
 from misc import check_and_expand_path, make_heading
 
-def record_dir(start_path, name, 
+def record_dir(inpath, name,
                include_checksum = False,
-               file_limit = 5000, 
-               logpath = 'state_of_input.log'):
+               file_limit = 5000,
+               outpath = 'state_of_input.log'):
     '''
-    Record relative path, size, and (optionally) checksum of all files within start_path.
-    Relative paths are from start_path.
-    Append info in |-delimited format to logpath below a heading made from start_path.
+    Record relative path, size, and (optionally) checksum of all files within inpath.
+    Relative paths are from inpath.
+    Append info in |-delimited format to outpath below a heading made from inpath.
     '''
-    check_logpath(logpath)
-    files_info = walk(start_path, include_checksum, file_limit)
-    write_log(name, files_info, logpath)
+    inpath, name, this_file_only, do_walk = check_inpath(inpath, name)
+    if do_walk:
+        files_info = walk(inpath, include_checksum, file_limit, this_file_only)
+    else:
+        files_info = None
+    check_outpath(outpath)
+    write_log(name, files_info, outpath)
     return None
 
-def check_logpath(logpath):
+def check_inpath(inpath, name):
     '''
-    Check that the directory for logpath exists
+    Check that inpath exists as file or directory.
+    If file, make inpath the file's directory and only record info for that file.
     '''
-    dirname = os.path.dirname(logpath)
-    if dirname:
-        check_and_expand_path(dirname)
+    this_file_only = None
+    do_walk = True
+    if os.path.isfile(inpath):
+        inpath = os.path.dirname(inpath)
+        this_file_only = os.path.abspath(inpath)
+    elif os.path.isdir(inpath):
+        pass
+    else:
+        name = name + ', could not find at runtime.'
+        do_walk = False
+    return inpath, name, this_file_only, do_walk
+
+def check_outpath(outpath):
+    '''
+    Ensure that the directory for outpath exists.
+    '''
+    dirname = os.path.dirname(outpath)
+    if dirname and not os.path.isdir(dirname):
+        os.makedirs(dirname)
     return None
 
-def walk(start_path, include_checksum, file_limit, this_file_only = None):
+def walk(inpath, include_checksum, file_limit, this_file_only):
     '''
-    Walk through start_path and grab paths to all subdirs and info on all files. 
+    Walk through inpath and grab paths to all subdirs and info on all files. 
     Walk in same order as os.walk.
     Keep walking until there are no more subdirs or there's info on file_limit files.
     '''
-    if os.path.isfile(start_path):
-        this_file_only = os.path.abspath(start_path)
-        start_path = os.path.dirname(start_path)
-    dirs = [start_path]
-    files_info = prep_files_info(include_checksum)
-    while dirs and len(files_info) <= file_limit:
+    dirs = [inpath]
+    files_info, file_limit = prep_files_info(include_checksum, file_limit)
+    while dirs and do_more_files(files_info, file_limit):
         dirs, files_info = scan_dir_wrapper(
-            dirs, files_info, start_path, include_checksum, file_limit, this_file_only)
+            dirs, files_info, inpath, include_checksum, file_limit, this_file_only)
     return files_info
 
-def prep_files_info(include_checksum):
+def prep_files_info(include_checksum, file_limit):
     '''
     Create a header for the file characteristics to grab.
+    Adjusts file_limit for existence of header. 
     '''
     files_info = [['file path', 'file size in bytes']]
     if include_checksum:
         files_info.append('MurmurHash3')
-    return files_info
+    file_limit += 1
+    return files_info, file_limit
 
-def scan_dir_wrapper(dirs, files_info, start_path, include_checksum, file_limit, 
+def do_more_files(files_info, file_limit):
+    '''
+    True if files_info has fewer then file_limit elements.
+    '''
+    return bool(len(files_info) < file_limit)
+
+def scan_dir_wrapper(dirs, files_info, inpath, include_checksum, file_limit, 
                      this_file_only):
     '''
     Drop down access and output management for scan_dir. 
@@ -60,11 +86,11 @@ def scan_dir_wrapper(dirs, files_info, start_path, include_checksum, file_limit,
     '''
     dir_to_scan = dirs.pop(0)
     subdirs, files_info = scan_dir(
-        dir_to_scan, files_info, start_path, include_checksum, file_limit, this_file_only)
+        dir_to_scan, files_info, inpath, include_checksum, file_limit, this_file_only)
     dirs += subdirs
     return dirs, files_info
 
-def scan_dir(dir_to_scan, files_info, start_path, include_checksum, file_limit, 
+def scan_dir(dir_to_scan, files_info, inpath, include_checksum, file_limit, 
              this_file_only):
     '''
     Collect names of all subdirs and all information on files.
@@ -77,19 +103,19 @@ def scan_dir(dir_to_scan, files_info, start_path, include_checksum, file_limit,
                 continue
             else:
                 subdirs.append(entry.path)
-        elif entry.is_file() and (not this_file_only or this_file_only == entry.path):
-            f_info = get_file_information(entry, start_path, include_checksum)
+        elif entry.is_file() and (this_file_only is None or this_file_only == entry.path):
+            f_info = get_file_information(entry, inpath, include_checksum)
             files_info.append(f_info)
-            if len(files_info) == file_limit:
+            if not do_more_files(files_info, file_limit):
                 break
     return subdirs, files_info
     
-def get_file_information(f, start_path, include_checksum):
+def get_file_information(f, inpath, include_checksum):
     '''
     Grabs path and size from scandir file object. 
     Will compute file's checksum if asked.
     '''
-    f_path = os.path.relpath(f.path, start_path).strip()
+    f_path = os.path.relpath(f.path, inpath).strip()
     f_size = str(f.stat().st_size)
     f_info = [f_path, f_size]
     if include_checksum:
@@ -98,16 +124,18 @@ def get_file_information(f, start_path, include_checksum):
         f_info.append(f_checksum)
     return f_info
 
-def write_log(name, files_info, logpath):
+def write_log(name, files_info, outpath):
     '''
-    Write file information to logpath under a nice header.
+    Write file information to outpath under a nice header.
     '''
     out_name = make_heading(name)
-    out_files_info = ['|'.join(l) for l in files_info]
-    out_files_info = '\n'.join(out_files_info)
-    with open(logpath, 'ab') as f:
+    if files_info is not None:
+        out_files_info = ['|'.join(l) for l in files_info]
+        out_files_info = '\n'.join(out_files_info)
+    else:
+        out_files_info = ''
+    with open(outpath, 'ab') as f:
         f.write(out_name)
-        f.write('\n')
         f.write(out_files_info)
         f.write('\n\n')
     return None
