2bd4ad416f5d5f8686d6da26f59112eb0a984b31
Veli Murat ANDIRIN <62054739+veli-m-andirin@users.noreply.github.com>
2022-03-31 12:57:25 -0400

    #172 extends functionality and adds unit tests

--- gslab_misc/SaveData/SaveData.py
+++ gslab_misc/SaveData/SaveData.py
@@ -7,23 +7,41 @@ Created on Sat Dec 25 05:44:33 2021
 
 import pandas as pd
 import hashlib
-import sys
+import re
 
-pd.set_option('display.float_format', lambda x: '%.5f' % x)
-
-def SaveData(df, keys, out_file, log_file):
+def SaveData(df, keys, out_file, log_file = '', append = False, sortbykey = True):
+    CheckExtension(out_file)
     CheckKeys(df, keys)
     df_hash = hashlib.md5(pd.util.hash_pandas_object(df).values).hexdigest() 
     summary_stats = GetSummaryStats(df)
-    SaveDf(df, out_file)
-    SaveLog(df_hash, keys, summary_stats, out_file, log_file)
+    SaveDf(df, keys, out_file, sortbykey)
+    SaveLog(df_hash, keys, summary_stats, out_file, append, log_file)
+    
+def CheckExtension(out_file):
+    extension = re.findall(r'\.[a-z]+$', out_file)
+    if not extension[0] in ['.csv', '.dta']:
+        raise ValueError("File extension should be one of .csv or .dta.")
     
 def CheckKeys(df, keys):
+    if not isinstance(keys, list):
+        raise TypeError("Keys must be specified as a list.")
+        
+    for key in keys:
+        if not key in df.columns:
+            print('%s is not a column name.' % (key))
+            raise ValueError('One of the keys you specified is not among the columns.')
+    
     df_keys = df[keys]
-    key_counts = df_keys.value_counts()
-    if not all(x == 1 for x in key_counts):
-        print("Keys do not uniquely identify the observations")
-        sys.exit()
+    
+    keys_with_missing = df_keys.columns[df_keys.isnull().any()]
+    if keys_with_missing.any():
+        missings_string = ', '.join(keys_with_missing)
+        print('The following keys are missing in some rows: %s .' % (missings_string))
+        raise ValueError('You have rows with missing keys.')
+    
+    
+    if not all(df.groupby(keys).size() == 1):
+        raise ValueError("Keys do not uniquely identify the observations.")
         
 def GetSummaryStats(df):
     var_types = df.dtypes
@@ -33,19 +51,41 @@ def GetSummaryStats(df):
     summary_stats = summary_stats.round(4)
     return(summary_stats)    
 
-def SaveDf(df, out_file):
-    df.to_csv(out_file, index = False)
+def SaveDf(df, keys, out_file, sortbykey):
+    if sortbykey:
+        df.sort_values(keys)
     
-def SaveLog(df_hash, keys, summary_stats, out_file, log_file):
-    with open(log_file, 'w') as f:
-        f.write(f'File: {out_file}\n\n')
-        f.write(f'MD5 hash: {df_hash}\n\n')
-        f.write('Keys: ')
-        for item in keys:
-            f.write(f'{item} ')
-        f.write('\n\n')
-        f.write(summary_stats.to_string(header = True, index = True))
-        f.write("\n")
+    if re.search('.csv', out_file):
+        df.to_csv(out_file, index = False)
+    if re.search('.dta', out_file):
+        df.to_stata(out_file, write_index = False)
+    
+def SaveLog(df_hash, keys, summary_stats, out_file, append, log_file):
+    if log_file: 
+        if append:
+            with open(log_file, 'a') as f:
+                f.write('\n\n')
+                f.write('File: %s\n\n' % (out_file))
+                f.write('MD5 hash: %s\n\n' % (df_hash))
+                f.write('Keys: ')
+                for item in keys:
+                    f.write('%s ' % (item))
+                f.write('\n\n')
+                f.write(summary_stats.to_string(header = True, index = True))
+                f.write("\n\n")
+        else:
+            with open(log_file, 'w') as f:
+                f.write('File: %s\n\n' % (out_file))
+                f.write('MD5 hash: %s\n\n' % (df_hash))
+                f.write('Keys: ')
+                for item in keys:
+                    f.write('%s ' % (item))
+                f.write('\n\n')
+                f.write(summary_stats.to_string(header = True, index = True))
+                f.write("\n\n")
         
-    f.close()
+        f.close()    
+    else:
+        pass
+    
 
---
+++ gslab_misc/SaveData/__init__.py
@@ -0,0 +1,7 @@
+'''
+SaveData: a Python library for saving data in a normalized format.
+=====================================================
+
+'''
+
+from SaveData import SaveData
