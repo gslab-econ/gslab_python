246b95ac08586bf1e12d825d5941c8586f10dea3
Chris Liao <33707455+liaochris@users.noreply.github.com>
2024-07-26 17:20:34 -0400

    update python test files #183

--- gslab_misc/SaveData/tests/test_main.py
+++ gslab_misc/SaveData/tests/test_main.py
@@ -8,55 +8,72 @@ sys.path.append('..')
 
 from SaveData import SaveData
 
+pd.set_option('future.no_silent_downcasting', True)
+
 class TestSaveData(unittest.TestCase):
     
     def test_wrong_extension(self):
-        df = pd.DataFrame({'a' : [4,3,6], 'b': [4,5,6], 'c': [1,2,3]})
+        df = pd.read_csv('data/data.csv')
         with self.assertRaises(ValueError):
-            SaveData(df, ['a'], 'dfs.pdf')
-            
+            SaveData(df, ['id'], 'dfs.pdf')
+
     def test_wrong_keytype(self):
-        df = pd.DataFrame({'a' : [4,3,6], 'b': [4,5,6], 'c': [1,2,3]})
+        df = pd.read_csv('data/data.csv')
         with self.assertRaises(TypeError):
-            SaveData(df, 'a', 'dfs.csv')        
+            SaveData(df, 'id', 'dfs.csv')        
             
     def test_missingkeys(self):
-        df = pd.DataFrame({'a' : [4,3,6], 'b': [4,np.nan,6], 'c': [1,2,3]})
+        df = pd.read_csv('data/data.csv')
         with self.assertRaises(ValueError):
-            SaveData(df, ['b'], 'dfs.csv')
+            SaveData(df, ['num'], 'dfs.csv')
 
     def test_duplicate_keys(self):
-        df = pd.DataFrame({'a' : [3,3,6], 'b': [4,5,6], 'c': [1,2,3]})
+        df = pd.read_csv('data/data.csv')
         with self.assertRaises(ValueError):
-            SaveData(df, ['a'], 'dfs.csv')
+            SaveData(df, ['partid1'], 'dfs.csv')
 
-    def test_saves_desired_file(self):
-        df = pd.DataFrame({'a' : [3,3,6], 'b': [4,5,6], 'c': [1,2,3]})
-        SaveData(df, ['b'], 'df.csv')
+    def test_multiple_keys(self):
+        df = pd.read_csv('data/data.csv')
+        SaveData(df, ['id', 'partid1','partid2'], 'df.csv')
+        df_saved = pd.read_csv('df.csv')
+        self.assertEqual(True, df.compare(df_saved).shape==(0,0))
+        os.remove('df.csv')
+
+    def test_saves_desired_file_dta(self):
+        df = pd.read_csv('data/data.csv')
+        SaveData(df, ['id'], 'df.dta')
+        df_saved = pd.read_stata('df.dta')
+        self.assertEqual(True, df.compare(df_saved).shape==(0,0))
+        os.remove('df.dta')
+                
+    def test_saves_desired_file_without_log(self):
+        df = pd.read_csv('data/data.csv')
+        SaveData(df, ['id'], 'df.csv')
         exists = os.path.isfile('df.csv')
-        self.assertEqual(exists, True)
+        df_saved = pd.read_csv('df.csv')
+        self.assertEqual(True, df.compare(df_saved).shape==(0,0))
         os.remove('df.csv')
                 
     def test_saves_with_log(self):    
-        df = pd.DataFrame({'a' : [3,3,6], 'b': [4,5,6], 'c': [1,2,3]})
-        SaveData(df, ['b'], 'df.csv', 'df.log')
+        df = pd.read_csv('data/data.csv')
+        SaveData(df, ['id'], 'df.csv', 'df.log')
         exists = os.path.isfile('df.log')
         self.assertEqual(exists, True)
         os.remove('df.log')
         os.remove('df.csv')
         
     def test_saves_when_append_given(self):    
-        df = pd.DataFrame({'a' : [3,3,6], 'b': [4,5,6], 'c': [1,2,3]})
-        SaveData(df, ['b'], 'df.csv', 'df.log')
-        SaveData(df, ['c'], 'df.csv', 'df.log', append = True)
+        df = pd.read_csv('data/data.csv')
+        SaveData(df, ['id'], 'df.csv')
+        SaveData(df, ['id'], 'df.csv', 'df.log', append = True)
         exists = os.path.isfile('df.log')
         self.assertEqual(exists, True)
         os.remove('df.log')
         os.remove('df.csv')
         
     def test_saves_when_sort_is_false(self):    
-        df = pd.DataFrame({'a' : [3,3,6], 'b': [4,5,6], 'c': [1,2,3]})
-        SaveData(df, ['c'], 'df.csv', 'df.log', sortbykey = False)
+        df = pd.read_csv('data/data.csv')
+        SaveData(df, ['id'], 'df.csv', 'df.log', sortbykey = False)
         exists = os.path.isfile('df.csv')
         self.assertEqual(exists, True)
         os.remove('df.log')
